b20069c57e79c299cd125b9aeb8efe9277ea4359
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-01-17 16:55:39 +0000

    modify task 6 files

--- models/__init__.py
+++ models/__init__.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
-'''
-    Package initializer
-'''
-import os
-from models.place import Place
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-
-classes = {"User": User, "BaseModel": BaseModel,
-           "Place": Place, "State": State,
-           "City": City, "Amenity": Amenity,
-           "Review": Review}
-
-storage_type = os.getenv("HBNB_TYPE_STORAGE")
-if storage_type == "db":
-    from models.engine.db_storage import DBStorage
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Defines State class"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 import models
-import os
 
 
 class State(BaseModel, Base):
-    """State class implementation"""
-    __tablename__ = 'states'
-
-    if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    """ State class """
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", passive_deletes=True)
+        cities = relationship("City", backref="state", cascade='all, delete')
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """Returns all cities in a State"""
-        return [city for city in State.cities if city.state_id == self.id]
+        @property
+        def cities(self):
+            """getter for list of citis"""
+            new_list = []
+            all_cities = models.storage.all(City)
+            for key in all_cities:
+                if all_cities[key] == self.id:
+                    new_list.append(all_cities[key])
+            return new_list
