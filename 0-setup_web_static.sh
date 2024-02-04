61821e4bd269c406b63b8900f432e65da2d748e1
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-04 13:12:10 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,6 @@ rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu: ubuntu /data/
 loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
-sudo sed -i "29i" $loc /etc/nginx/sites-enabled/default
+sudo sed -i "59i" $loc /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
