98f89ccec0369af28627b333537bf94315295216
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-04 13:04:28 -0800

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update -y
+sudo apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" >> /data/web_static/releases/test/index.html
+rm -rf /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu: ubuntu /data/
+loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
+sudo sed -i "29i" $loc /etc/nginx/sites-enabled/default
+sudo service nginx restart
+exit 0
