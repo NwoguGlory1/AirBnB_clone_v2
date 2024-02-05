894acb95335e114bfbd48c1eea781353ae3c58cf
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-04 19:00:11 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,19 @@
 
 sudo apt-get update -y
 sudo apt-get install -y nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" >> /data/web_static/releases/test/index.html
-rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu: ubuntu /data/
-loc="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n"
-sudo sed -i "59i" $loc /etc/nginx/sites-enabled/default
-sudo service nginx restart
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" >> sudo tee /data/web_static/releases/test/index.html
+sudo rm -rf /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+echo "Chown completed successfully"
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+} " | sudo tee /etc/nginx/sites-available/default
+sudo nginx -t && sudo service nginx restart
 exit 0
