ce3eaee5e4c9dca647527cea05e22e3aaf2d2e10
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-04 20:16:38 -0800

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Script that generates a .tgz archive from the contents of the web_static folder of AirBnB clone, using do_pack
+
+fro web_static import task,
+@task
+def do_pack():
+    try:
+        Today = datetime.now().strftime('%Y%m%d%H%M%S')
+        path = "versions/web_static_{:s}.tgz".format(Today)
+
+        msg1 = "Packing web_static to {:s}".format(path)
+        print(msg1)
+
+        with hide('running'):
+            local('mkdir -p ./versions')
+
+        local('tar -cvzf {:s} web_static'.format(path))
+
+        with hide('running'):
+            size = local('wc -c < {:s}'.format(path), capture=True)
+
+        msg2 = 'web_static packed: {:s} -> {:s}Bytes'.format(path, size)
+        print(msg2)
+
+        return path
+
+    except:
+        return None
+
