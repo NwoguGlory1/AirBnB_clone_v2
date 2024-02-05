627c8621bebc01257310afd3c265384b6550d45d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-05 03:28:00 -0800

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,23 +10,26 @@ def do_pack():
     Generates a .tgz archive from the contents
     of the web_static folder
     """
+    """ Define the timestamp and path for the archive"""
     try:
         Today = datetime.now().strftime('%Y%m%d%H%M%S')
         path = "versions/web_static_{:s}.tgz".format(Today)
 
-        msg1 = "Packing web_static to {:s}".format(path)
-        print(msg1)
-
+        """ Prints a message to indcate start of packing process"""
+        print("Packing web_static to {:s}".format(path))
+        
+        """ Hides output of the versions directries created if it doesnt exist"""
         with hide('running'):
             local('mkdir -p ./versions')
-
+ 
+        """ creates archive with cvzf locally"""
         local('tar -cvzf {:s} web_static'.format(path))
 
         with hide('running'):
+        """ counts the no of bytes in path ^ capture captures byte count"""
             size = local('wc -c < {:s}'.format(path), capture=True)
 
-        msg2 = 'web_static packed: {:s} -> {:s}Bytes'.format(path, size)
-        print(msg2)
+        print('web_static packed: {:s} -> {:s}Bytes'.format(path, size))
 
         return path
 
