c5204743e1792d559268fa6eab6baeb6143f0d2d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-04 19:54:40 -0800

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,9 +13,21 @@ echo "Chown completed successfully"
 printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 } " | sudo tee /etc/nginx/sites-available/default
 sudo nginx -t && sudo service nginx restart
 exit 0
