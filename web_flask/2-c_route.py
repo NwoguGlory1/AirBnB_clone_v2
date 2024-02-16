914f8d78d4e5c759631072c97833e680d8d543eb
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-16 13:40:34 -0800

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+""" imports Flask webframe """
+
+app = Flask(__name__)
+""" Creates an instance of class, Flask"""
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Function that executes if user accesses '/' URL"""
+    return f'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ Function that exceutes if user accesses '/hbnb' """
+    return f'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_C(text):
+    """ Function that exceutes if user accesses '/c/<text> """
+   new_text = text.replace('_', ' ')
+    return f'C {new_text}'
+
+
+if __name__ == '__main__':
+    """
+    Ensures script runs only when excuted directly,
+    not when imported as a module in another script.
+    """
+    app.run(host='0.0.0.0', port=5000, debug=True)
