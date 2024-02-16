a159f9d11000be24588d2e9f9d3cd01db3f027f9
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-16 11:34:09 -0800

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask
+""" imports Flask webframe """
+
+app = Flask(__name__)
+""" Creates an instance of class, Flask"""
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Function that executes if user accesses '/' URL"""
+    return f'Hello, HBNB!'
+
+if __name__ == '__main__':
+    """
+    Ensures script runs only when excuted directly,
+    not when imported as a module in another script.
+    """
+    app.run(host='0.0.0.0', port=5000, debug=True)
