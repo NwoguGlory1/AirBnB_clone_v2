bcf856e6ac66f7713c0667548c82d98bd81023b6
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-17 14:13:11 -0800

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -34,7 +34,7 @@ def display_Python(text='is cool'):
     return f'Python {new_text}'
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def display_n(n):
     """ Function that executes if user accesses '/number/<n>' """
     if n.isdigit():
