380b141bd0fec50726c969912a709b5b2bf4f9da
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-17 09:22:56 -0800

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,27 +6,38 @@ from flask import Flask
 app = Flask(__name__)
 """ Creates an instance of class, Flask"""
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """ Function that executes if user accesses '/' URL"""
     return f'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """ Function that exceutes if user accesses '/hbnb' """
     return f'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def display_C(text):
     """ Function that exceutes if user accesses '/c/<text> """
-   new_text = text.replace('_', ' ')
+    new_text = text.replace('_', ' ')
     return f'C {new_text}'
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def display_Python(text='is cool'):
     """ Function that executes if user accesses '/python/<text>' """
     new_text = text.replace('_', ' ')
     return f'Python {'new_text'} is cool'
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def display_n():
+    """ Function that executes if user accesses '/number/<n>' """
+    return f'Python {'new_text'} is cool'
+
 
 if __name__ == '__main__':
     """
