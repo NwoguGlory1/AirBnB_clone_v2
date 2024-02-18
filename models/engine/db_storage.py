75d0460a69a9ad4dcf76f16d39028f7d7594a6a2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-18 09:51:51 -0800

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,3 +65,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
+
+    def close(self):
+        """ Public method to close the session"""
+        self.__session.remove()
